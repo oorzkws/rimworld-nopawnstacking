396a07cddb0c6fae8a7c6c6f4afdb3d7e048c9a0
oorzkws <65210810+oorzkws@users.noreply.github.com>
2023-05-24 16:09:53 -0600

    Optimize build

--- Source/AssemblyInfo.cs
+++ Source/AssemblyInfo.cs
@@ -4,14 +4,14 @@ using System.Reflection;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Mod Template")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Mod Template")]
-[assembly: AssemblyCopyright("©  oorzkws_l")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle("nopawnstacking")]
+//[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("Release")]
+//[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Combat Always Collides")]
+[assembly: AssemblyCopyright("©  Mute")]
+//[assembly: AssemblyTrademark("")]
+//[assembly: AssemblyCulture("")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -24,4 +24,4 @@ using System.Reflection;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.4.0.0")]
-[assembly: AssemblyFileVersion("23.05.14.055344")]
+[assembly: AssemblyFileVersion("23.05.24.040831")]
