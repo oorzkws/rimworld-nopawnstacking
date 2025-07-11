2f58b93dc164e608e637873aedf9fd88002ca583
oorzkws <65210810+oorzkws@users.noreply.github.com>
2025-07-11 17:16:12 -0600

    1.6?
    Dunno if this is perfect yet

--- Source/Main.cs
+++ Source/Main.cs
@@ -10,6 +10,7 @@ using Unity.Mathematics;
 using UnityEngine;
 using Verse;
 using Verse.AI;
+using CodeMatch = HarmonyLib.CodeMatch;
 
 namespace nopawnstacking;
 
@@ -74,7 +75,7 @@ public static class HarmonyPatches {
         }
     }
     
-    [HarmonyPatch(typeof(Pawn_MindState), "MindStateTick"), UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+    [HarmonyPatch(typeof(Pawn_MindState), "MindStateTickInterval"), UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
     public static class MindStateTickPatch {
         public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator) {
             var editor = new CodeMatcher(instructions, generator);
@@ -87,14 +88,15 @@ public static class HarmonyPatches {
                 new CodeMatch(OpCodes.Ldc_I4_S), // 18
                 new CodeMatch(OpCodes.Br_S),
                 new CodeMatch(OpCodes.Ldc_I4_S), // 24
-                new CodeMatch(OpCodes.Stloc_1),
+                new CodeMatch(OpCodes.Stloc_2),
                 new CodeMatch(OpCodes.Ldarg_0),
                 new CodeMatch(OpCodes.Ldarg_0),
                 new CodeMatch(OpCodes.Ldfld, AccessTools.Field(typeof(Pawn_MindState), "pawn")),
-                new CodeMatch(OpCodes.Ldloc_1),
+                new CodeMatch(OpCodes.Ldloc_2),
                 new CodeMatch(OpCodes.Ldc_I4_1),
                 new CodeMatch(OpCodes.Ldc_R4), // -1
                 new CodeMatch(OpCodes.Ldc_I4_1),
+                new CodeMatch(OpCodes.Ldc_I4_0),
                 new CodeMatch(OpCodes.Call, AccessTools.Method(typeof(PawnUtility), "EnemiesAreNearby"))
             };
             editor.Start().MatchStartForward(matchPattern);
@@ -152,6 +154,91 @@ public static class HarmonyPatches {
             }
 
             Log.Error("[Combat Always Collides] Failed to apply pathertick patch");
+            return editor.InstructionEnumeration();
+        }
+    }
+
+    [HarmonyPatch(typeof(PawnUtility), "AnyPawnBlockingPathAt")]
+    public static class AnyPawnBlockingPathAtPatch {
+        internal static bool ShouldActAsIfHadCollideWithPawnsJob(Pawn pawn, bool defaultReturn) {
+            if (defaultReturn)
+                return true;
+            if (pawn.mindState is not { } mindState)
+                return false;
+            return mindState.anyCloseHostilesRecently;
+        }
+        internal static bool ShouldCollideOnlyWithStandingPawns(Pawn pawn, bool defaultReturn) {
+            if (!defaultReturn || pawn.mindState is not { } mindState)
+                return defaultReturn;
+            return !mindState.anyCloseHostilesRecently;
+        }
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator) {
+            var editor = new CodeMatcher(instructions, generator);
+            // --------------------------ORIGINAL--------------------------
+            // PawnBlockingPathAt(c, forPawn, actAsIfHadCollideWithPawnsJob, collideOnlyWithStandingPawns, forPathFinder, useId)
+            var matchPattern = new[] { // Just grabbing the two args we want to change
+                new CodeMatch(OpCodes.Ldarg_2),
+                new CodeMatch(OpCodes.Ldarg_3),
+            };
+            editor.Start().MatchStartForward(matchPattern);
+
+            if (editor.IsValid) {
+                // --------------------------MODIFIED--------------------------
+                // PawnBlockingPathAt(c, forPawn, actAsIfHadCollideWithPawnsJob || forPawn.mindState?.anyCloseHostilesRecently, collideOnlyWithStandingPawns && !forPawn.mindState?.anyCloseHostilesRecently, forPathFinder, useId)
+                return editor
+                    .RemoveInstructions(matchPattern.Length)
+                    .InsertAndAdvance(
+                        new CodeInstruction(OpCodes.Ldarg_1),
+                        new CodeInstruction(OpCodes.Ldarg_2),
+                        new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(AnyPawnBlockingPathAtPatch), "ShouldActAsIfHadCollideWithPawnsJob")),
+                        new CodeInstruction(OpCodes.Ldarg_1),
+                        new CodeInstruction(OpCodes.Ldarg_3),
+                        new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(AnyPawnBlockingPathAtPatch), "ShouldCollideOnlyWithStandingPawns"))
+                    )
+                    .InstructionEnumeration();
+            }
+
+            Log.Error("[Combat Always Collides] Failed to apply AnyPawnBlockingPathAt patch");
+            return editor.InstructionEnumeration();
+        }
+    }
+    
+    [HarmonyPatch(typeof(Pawn_PathFollower), "WillCollideWithPawnAt")]
+    public static class WillCollideWithPawnAtPatch {
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator) {
+            var editor = new CodeMatcher(instructions, generator);
+            // --------------------------ORIGINAL--------------------------
+            // actAsIfIHadCollideWithPawnsJob: false, forceOnlyStanding || (pawn.IsShambler && !pawn.mindState.anyCloseHostilesRecently)
+            var matchPattern = new[] { // Just grabbing the two args we want to change
+                new CodeMatch(OpCodes.Ldc_I4_0),
+                new CodeMatch(OpCodes.Ldarg_2),
+                new CodeMatch(OpCodes.Brtrue_S),
+                new CodeMatch(OpCodes.Ldarg_0),
+                new CodeMatch(OpCodes.Ldfld), // PathFollower::pawn
+                new CodeMatch(OpCodes.Callvirt), // get_IsShambler()
+            };
+            editor.Start().MatchStartForward(matchPattern);
+
+            if (editor.IsValid) {
+                // --------------------------MODIFIED--------------------------
+                // actAsIfIHadCollideWithPawnsJob: pawn.mindState.anyCloseHostilesRecently, (forceOnlyStanding && !pawn.mindState.anyCloseHostilesRecently)
+                return editor
+                    .Advance(0)
+                    .SetAndAdvance(OpCodes.Ldarg_0, null)
+                    .InsertAndAdvance(
+                        new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(Pawn_PathFollower), "pawn")),
+                        new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(Pawn), "mindState")),
+                        new CodeInstruction(OpCodes.Ldfld, AccessTools.Field(typeof(Pawn_MindState), "anyCloseHostilesRecently"))
+                    )
+                    .Advance(1) // Step past the ldarg_2
+                    .RemoveInstructions(4) // remove brtrue through callvirt
+                    .MatchStartForward(new CodeMatch(OpCodes.Ldc_I4_1)) // Move to the now-errant ldc_i4_1 and remove it, it's an orphaned jump
+                    .ThrowIfFalse("Second return statement not found, please report this", cm => cm.IsValid)
+                    .RemoveInstruction()
+                    .InstructionEnumeration();
+            }
+
+            Log.Error("[Combat Always Collides] Failed to apply WillCollideWithPawnAt patch");
             return editor.InstructionEnumeration();
         }
     }
